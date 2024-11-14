fe72a83e034864567d467c5e755e230dd282e399
Edil Medeiros <j.edil@ene.unb.br>
2024-11-13 23:24:28 -0300

    Refactor drivers

--- src/Driver.hs
+++ src/Driver.hs
@@ -15,12 +15,12 @@ epslon = 0.00001
 -- | Run the simulation and return the result in the last 
 -- time point using the specified simulation specs.
 runCTFinal :: Model a -> Double -> Solver -> IO a
-runCTFinal m t sl = 
-  do d <- runReaderT m $ Parameters { interval = Interval 0 t,
-                                      time = 0,
-                                      iteration = 0,
-                                      solver = sl { stage = SolverStage 0 }}
-     subRunCTFinal d t sl
+runCTFinal m t sl = do
+  d <- runReaderT m $ Parameters { interval = Interval 0 t,
+                                   time = 0,
+                                   iteration = 0,
+                                   solver = sl { stage = SolverStage 0 }}
+  subRunCTFinal d t sl
 
 -- | Auxiliary functions to runCTFinal
 subRunCTFinal :: CT a -> Double -> Solver -> IO a
@@ -28,16 +28,16 @@ subRunCTFinal m t sl = do
   let iv = Interval 0 t
       n = iterationHiBnd iv (dt sl)
       disct = iterToTime iv sl n (SolverStage 0)
-      x = runReaderT m $ Parameters { interval = iv,
-                                      time = disct,
-                                      iteration = n,
-                                      solver = sl { stage = SolverStage 0 }}
-  if disct - t < epslon
-  then x
-  else runReaderT m $ Parameters { interval = iv,
-                                   time = t,
-                                   iteration = n,
-                                   solver = sl { stage = Interpolate }}
+      ps = if disct - t < epslon
+           then Parameters { interval = iv,
+                             time = disct,
+                             iteration = n,
+                             solver = sl { stage = SolverStage 0 }}
+           else Parameters { interval = iv,
+                             time = t,
+                             iteration = n,
+                             solver = sl { stage = Interpolate }}
+  runReaderT m ps
 
 -- | Run the simulation and return the results in all 
 -- integration time points using the specified simulation specs.
@@ -57,19 +57,17 @@ subRunCT m t sl = do
       parameterize n =
         let time = iterToTime iv sl n (SolverStage 0)
             solver = sl {stage = SolverStage 0}
-        in
-        Parameters {interval = iv,
-                    time = time,
-                    iteration = n,
-                    solver = solver}
-      ps =
-       Parameters {interval = iv,
-                   time = t,
-                   iteration = nu,
-                   solver = sl {stage = Interpolate}}
-      endTime = iterToTime iv sl nu (SolverStage 0)
-      values = map (runReaderT m . parameterize) [nl .. nu]
-  if endTime - t < epslon
-  then values
-  else init values ++ [runReaderT m ps]
+        in Parameters { interval = iv,
+                        time = time,
+                        iteration = n,
+                        solver = solver }
 
+      disct = iterToTime iv sl nu (SolverStage 0)
+      values = map (runReaderT m . parameterize) [nl .. nu]
+  if disct - t < epslon
+  then values
+  else let ps = Parameters { interval = iv,
+                             time = t,
+                             iteration = nu,
+                             solver = sl {stage = Interpolate} }
+       in init values ++ [runReaderT m ps]
