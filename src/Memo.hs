9905eee7c3b984a1a8ac88a87b837254ef8f2568
Edil Medeiros <j.edil@ene.unb.br>
2024-11-13 23:07:06 -0300

    Refactor memo

--- src/Memo.hs
+++ src/Memo.hs
@@ -16,7 +16,8 @@ import Data.IORef ( newIORef, readIORef, writeIORef )
 import Data.Array ( Ix )
 import Data.Array.IO
     ( Ix, readArray, writeArray, MArray(newArray_), IOUArray, IOArray )
-import Control.Monad.Trans.Reader ( ReaderT(ReaderT, runReaderT) )
+import Control.Monad.Trans.Reader
+import Control.Monad.IO.Class
 
 -- -- | The 'Memo' class specifies a type for which an array can be created.
 class (MArray IOArray e IO) => Memo e where
@@ -33,41 +34,43 @@ instance (MArray IOUArray e IO) => UMemo e where
   newMemoUArray_ = newArray_
 
 -- | Memoize and order the computation in the integration time points using 
--- the specified interpolation and being aware of the Runge-Kutta method.
-memo :: UMemo e => (CT e -> CT e) -> CT e 
-        -> CT (CT e)
-memo tr m =
-  ReaderT $ \ps -> do
+-- the specified interpolation and being aware of the Runge-Kutta method.m
+memo :: UMemo e => (CT e -> CT e) -> CT e -> CT (CT e)
+memo interpolate m = do
+  ps <- ask
   let sl = solver ps
       iv = interval ps
       (SolverStage stl, SolverStage stu) = stageBnds sl
       (nl, nu)   = iterationBnds iv (dt sl)
-  arr   <- newMemoUArray_ ((stl, nl), (stu, nu))
-  nref  <- newIORef 0
-  stref <- newIORef 0
-  let r ps = do
+  arr   <- liftIO $ newMemoUArray_ ((stl, nl), (stu, nu))
+  nref  <- liftIO $ newIORef 0
+  stref <- liftIO $ newIORef 0
+  let r = do
+        ps <- ask
         let sl  = solver ps
             iv  = interval ps
             n   = iteration ps
             st  = getSolverStage $ stage sl
             stu = getSolverStage $ stageHiBnd sl 
-            loop n' st' = 
-              if (n' > n) || ((n' == n) && (st' > st)) 
+            loop n' stage' =
+              -- we have already memoized all values from 0 to n and from 0 to
+              -- st, just look up.
+              if (n' > n) || ((n' == n) && (stage' > st))
               then 
                 readArray arr (st, n)
               else 
-                let ps' = ps { time = iterToTime iv sl n' (SolverStage st'),
+                let ps' = ps { time = iterToTime iv sl n' (SolverStage stage'),
                                iteration = n',
-                               solver = sl { stage = SolverStage st' }}
+                               solver = sl { stage = SolverStage stage' }}
                 in do a <- runReaderT m ps'
-                      a `seq` writeArray arr (st', n') a
-                      if st' >= stu 
+                      a `seq` writeArray arr (stage', n') a
+                      if stage' >= stu
                         then do writeIORef stref 0
                                 writeIORef nref (n' + 1)
                                 loop (n' + 1) 0
-                        else do writeIORef stref (st' + 1)
-                                loop n' (st' + 1)
-        n'  <- readIORef nref
-        st' <- readIORef stref
-        loop n' st'
-  pure . tr . ReaderT $ r
+                        else do writeIORef stref (stage' + 1)
+                                loop n' (stage' + 1)
+        n'  <- liftIO $ readIORef nref
+        st' <- liftIO $ readIORef stref
+        liftIO $ loop n' st'
+  pure . interpolate $ r
