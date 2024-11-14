ad80e0dee0e691e8e717a6e489edb61085668c47
Edil Medeiros <j.edil@ene.unb.br>
2024-11-13 21:00:03 -0300

    Refactor interpolate
    We now use the ReaderT abstractions instead of manually building the
    computation.

--- src/Interpolation.hs
+++ src/Interpolation.hs
@@ -8,7 +8,7 @@ import Solver
       Stage(SolverStage, Interpolate),
       getSolverStage,
       iterToTime )
-import Control.Monad.Trans.Reader ( ReaderT(ReaderT, runReaderT) )
+import Control.Monad.Trans.Reader
 
 -- | Function to solve floating point approximations
 neighborhood :: Solver -> Double -> Double -> Bool
@@ -39,10 +39,10 @@ discrete m =
 
 -- | Interpolate the computation based on the integration time points only.
 interpolate :: CT Double -> CT Double
-interpolate m =
-  ReaderT $ \ps ->
+interpolate m = do
+  ps <- ask
   case stage $ solver ps of
-    SolverStage _ -> runReaderT m ps
+    SolverStage _ -> m
     Interpolate   ->
       let iv = interval ps
           sl = solver ps
@@ -53,14 +53,12 @@ interpolate m =
           n2 = min (ceiling x) (iterationHiBnd iv st)
           t1 = iterToTime iv sl n1 (SolverStage 0)
           t2 = iterToTime iv sl n2 (SolverStage 0)
-          z1 =
-            runReaderT m $ ps { time = t1,
-                                iteration = n1,
-                                solver = sl { stage = SolverStage 0 }}
-          z2 =
-            runReaderT m $ ps { time = t2,
-                                iteration = n2,
-                                solver = sl { stage = SolverStage 0 }}         
-      in do y1 <- z1
-            y2 <- z2
-            return $ y1 + (y2 - y1) * (t - t1) / (t2 - t1)
+          ps1 = ps { time = t1,
+                     iteration = n1,
+                     solver = sl { stage = SolverStage 0 }}
+          ps2 = ps { time = t2,
+                     iteration = n2,
+                     solver = sl { stage = SolverStage 0 }}
+          z1 = local (const ps1) m
+          z2 = local (const ps2) m
+      in z1 + (z2 - z1) * pure ((t - t1) / (t2 - t1))
